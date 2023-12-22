6de964bc9952428d65d348ddd59480f6ee8fdf3f
cocorocho <cobanerkan@yahoo.com>
2023-12-22 11:52:11 -0300

    fix node static files only being read on dev, minify tailwind

--- server/clerq/clerq/settings/base.py
+++ server/clerq/clerq/settings/base.py
@@ -132,7 +132,10 @@ STATIC_URL = "static/"
 STATIC_ROOT = "/var/www"
 STATICFILES_DIRS = [
     BASE_DIR / "staticfiles",
+    BASE_DIR / "node_tools" / "node_modules",
 ]
+MEDIA_ROOT = BASE_DIR / "mediafiles"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- server/clerq/clerq/settings/dev.py
+++ server/clerq/clerq/settings/dev.py
@@ -7,12 +7,6 @@ INSTALLED_APPS += [
     "debug_toolbar",
 ]
 
-# Static / Media
-STATICFILES_DIRS += [
-    BASE_DIR / "node_tools" / "node_modules",
-]
-MEDIA_ROOT = BASE_DIR / "mediafiles"
-
 # Middlewares
 MIDDLEWARE.insert(4, "debug_toolbar.middleware.DebugToolbarMiddleware")
 
